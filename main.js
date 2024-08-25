ff724509f075622e5018c8b20a8669184a38ab9b
Wojciech <wojciechwroobel@gmail.com>
2024-08-25 23:09:50 +0200

    styling tasks for future use in js, used selectors to declare variables

---
+++ main.js
@@ -0,0 +1,12 @@
+const doneTaskList = document.querySelector('#done-task-list');
+const doingTaskList = document.querySelector('#doing-task-list');
+const toDoTaskList = document.querySelector('#to-do-task-list');
+const taskNameInput = document.querySelector('#task-name-input');
+const addTaskButton = document.querySelector('#add-task-button');
+
+if (taskNameInput.value !== '') {
+    addTaskButton.addEventListener('click', function() {
+
+    })
+}
+
