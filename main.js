b2edd141e941792a7e047d6e53030e0b046d6bfa
Wojciech <wojciechwroobel@gmail.com>
2024-08-26 21:35:30 +0200

    created classes in JS for variables, used 'append' to assign elements

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const newTask = taskNameInput.value;
 
 newTask.forEach(function(task) {
     const taskContainer = document.createElement('div');
-    const taskName = document.createElement('h2');
+    const taskName = document.createElement('h3');
     const taskIconContainer = document.createElement('div');
     const leftArrowButton = document.createElement('button');
     const rightArrowButton = document.createElement('button');
@@ -17,10 +17,33 @@ newTask.forEach(function(task) {
     const rightArrowIcon = document.createElement('i');
     const trashCanIcon = document.createElement('i');
 
-})
+    taskContainer.classList.add('task');
+    taskName.innerText = taskNameInput.value;
+    taskName.classList.add('task-list-header');
+    taskIconContainer.classList.add('task-icons');
+    leftArrowButton.classList.add('arrow-left-button', 'task-button', 'hidden-button');
+    trashCanButton.classList.add('trash-can-button', 'task-button');
+    rightArrowButton.classList.add('arrow-right-button', 'task-button');
+    leftArrowIcon.classList.add('fa-solid', 'fa-circle-arrow-left', 'arrow-left-icon', 'icon');
+    trashCanIcon.classList.add('fa-solid', 'fa-trash-can', 'trash-can-icon', 'icon');
+    rightArrowIcon.classList.add('fa solid', 'fa-circle-arrow-right', 'arrow-right-icon', 'icon');
 
-if (taskNameInput.value !== '') {
-    addTaskButton.addEventListener('click', function() {
-
+    trashCanButton.addEventListener('click', function () {
+        taskContainer.remove();
     })
-}
+
+    rightArrowButton.append(rightArrowIcon);
+    trashCanButton.append(trashCanIcon);
+    leftArrowButton.append(leftArrowIcon);
+    taskIconContainer.append(leftArrowButton, trashCanButton, rightArrowButton);
+    taskContainer.append(taskName, taskIconContainer);
+
+
+
+    if (taskNameInput.value !== '') {
+        addTaskButton.addEventListener('click', function() {
+
+        })
+    }
+
+})
