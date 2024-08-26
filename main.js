0874021d9086faaa234cf5c6a24fed64ca9a8909
Wojciech <wojciechwroobel@gmail.com>
2024-08-26 20:21:59 +0200

    declared new variables creating elements, created new class for future hidden button

--- main.js
+++ main.js
@@ -4,9 +4,23 @@ const toDoTaskList = document.querySelector('#to-do-task-list');
 const taskNameInput = document.querySelector('#task-name-input');
 const addTaskButton = document.querySelector('#add-task-button');
 
+const newTask = taskNameInput.value;
+
+newTask.forEach(function(task) {
+    const taskContainer = document.createElement('div');
+    const taskName = document.createElement('h2');
+    const taskIconContainer = document.createElement('div');
+    const leftArrowButton = document.createElement('button');
+    const rightArrowButton = document.createElement('button');
+    const trashCanButton = document.createElement('button');
+    const leftArrowIcon = document.createElement('i');
+    const rightArrowIcon = document.createElement('i');
+    const trashCanIcon = document.createElement('i');
+
+})
+
 if (taskNameInput.value !== '') {
     addTaskButton.addEventListener('click', function() {
 
     })
 }
-
