7c626e68779568eaf541dad3c807a7a114ef7705
Wojciech <wojciechwroobel@gmail.com>
2024-08-27 17:47:37 +0200

    fixed left arrow in done column, changed if not to put everything inside it - everything is working

--- main.js
+++ main.js
@@ -7,68 +7,69 @@ const addTaskButton = document.querySelector('#add-task-button');
 addTaskButton.addEventListener('click', function() {
     const newTask = taskNameInput.value;
 
-    if (newTask !== '') {
-        const taskContainer = document.createElement('div');
-        const taskName = document.createElement('h3');
-        const taskIconContainer = document.createElement('div');
-        const leftArrowButton = document.createElement('button');
-        const rightArrowButton = document.createElement('button');
-        const trashCanButton = document.createElement('button');
-        const leftArrowIcon = document.createElement('i');
-        const rightArrowIcon = document.createElement('i');
-        const trashCanIcon = document.createElement('i');
-
-        taskContainer.classList.add('task');
-        taskName.innerText = taskNameInput.value;
-        taskName.classList.add('task-list-header');
-        taskIconContainer.classList.add('task-icons');
-        leftArrowButton.classList.add('arrow-left-button', 'task-button', 'hidden-button');
-        trashCanButton.classList.add('trash-can-button', 'task-button');
-        rightArrowButton.classList.add('arrow-right-button', 'task-button');
-        leftArrowIcon.classList.add('fa-solid', 'fa-circle-arrow-left', 'arrow-left-icon', 'icon');
-        trashCanIcon.classList.add('fa-solid', 'fa-trash-can', 'trash-can-icon', 'icon');
-        rightArrowIcon.classList.add('fa-solid', 'fa-circle-arrow-right', 'arrow-right-icon', 'icon');
-
-        trashCanButton.addEventListener('click', function() {
-            taskContainer.remove();
-        })
-
-        rightArrowButton.append(rightArrowIcon);
-        trashCanButton.append(trashCanIcon);
-        leftArrowButton.append(leftArrowIcon);
-        taskIconContainer.append(leftArrowButton, trashCanButton, rightArrowButton);
-        taskContainer.append(taskName, taskIconContainer);
-        toDoTaskList.append(taskContainer);
-
-        rightArrowButton.addEventListener('click', function() {
-            doingTaskList.append(taskContainer);
-            const doingRightArrowButton = rightArrowButton.cloneNode(true);
-            const doingLeftArrowButton = leftArrowButton.cloneNode(true);
-
-            rightArrowButton.replaceWith(doingRightArrowButton);
-            leftArrowButton.replaceWith(doingLeftArrowButton);
-            doingLeftArrowButton.classList.remove('hidden-button');
-
-            doingLeftArrowButton.addEventListener('click', function() {
-                toDoTaskList.append(taskContainer);
-                doingLeftArrowButton.replaceWith(leftArrowButton);
-                doingRightArrowButton.replaceWith(rightArrowButton);
-                leftArrowButton.classList.add('hidden-button');
-            })
-
-            doingRightArrowButton.addEventListener('click', function() {
-                doneTaskList.append(taskContainer);
-                const doneLeftArrowButton = doingLeftArrowButton.cloneNode(true);
-                leftArrowButton.replaceWith(doneLeftArrowButton);
-                doingRightArrowButton.classList.add('hidden-button');
-
-                doneLeftArrowButton.addEventListener('click', function() {
-                    doingTaskList.append(taskContainer);
-                    doneLeftArrowButton.replaceWith(doingLeftArrowButton);
-                    doingRightArrowButton.classList.remove('hidden-button');
-                })
-            })
-        })
+    if (!newTask) {
+        return;
     }
-});
 
+    const taskContainer = document.createElement('div');
+    const taskName = document.createElement('h3');
+    const taskIconContainer = document.createElement('div');
+    const leftArrowButton = document.createElement('button');
+    const rightArrowButton = document.createElement('button');
+    const trashCanButton = document.createElement('button');
+    const leftArrowIcon = document.createElement('i');
+    const rightArrowIcon = document.createElement('i');
+    const trashCanIcon = document.createElement('i');
+
+    taskContainer.classList.add('task');
+    taskName.innerText = taskNameInput.value;
+    taskName.classList.add('task-list-header');
+    taskIconContainer.classList.add('task-icons');
+    leftArrowButton.classList.add('arrow-left-button', 'task-button', 'hidden-button');
+    trashCanButton.classList.add('trash-can-button', 'task-button');
+    rightArrowButton.classList.add('arrow-right-button', 'task-button');
+    leftArrowIcon.classList.add('fa-solid', 'fa-circle-arrow-left', 'arrow-left-icon', 'icon');
+    trashCanIcon.classList.add('fa-solid', 'fa-trash-can', 'trash-can-icon', 'icon');
+    rightArrowIcon.classList.add('fa-solid', 'fa-circle-arrow-right', 'arrow-right-icon', 'icon');
+
+    trashCanButton.addEventListener('click', function() {
+        taskContainer.remove();
+    })
+
+    rightArrowButton.append(rightArrowIcon);
+    trashCanButton.append(trashCanIcon);
+    leftArrowButton.append(leftArrowIcon);
+    taskIconContainer.append(leftArrowButton, trashCanButton, rightArrowButton);
+    taskContainer.append(taskName, taskIconContainer);
+    toDoTaskList.append(taskContainer);
+
+    rightArrowButton.addEventListener('click', function() {
+        doingTaskList.append(taskContainer);
+        const doingRightArrowButton = rightArrowButton.cloneNode(true);
+        const doingLeftArrowButton = leftArrowButton.cloneNode(true);
+
+        rightArrowButton.replaceWith(doingRightArrowButton);
+        leftArrowButton.replaceWith(doingLeftArrowButton);
+        doingLeftArrowButton.classList.remove('hidden-button');
+
+        doingLeftArrowButton.addEventListener('click', function() {
+            toDoTaskList.append(taskContainer);
+            doingLeftArrowButton.replaceWith(leftArrowButton);
+            doingRightArrowButton.replaceWith(rightArrowButton);
+            leftArrowButton.classList.add('hidden-button');
+        })
+
+        doingRightArrowButton.addEventListener('click', function() {
+            doneTaskList.append(taskContainer);
+            const doneLeftArrowButton = doingLeftArrowButton.cloneNode(true);
+            doingLeftArrowButton.replaceWith(doneLeftArrowButton);
+            doingRightArrowButton.classList.add('hidden-button');
+
+            doneLeftArrowButton.addEventListener('click', function() {
+                doingTaskList.append(taskContainer);
+                doneLeftArrowButton.replaceWith(doingLeftArrowButton);
+                doingRightArrowButton.classList.remove('hidden-button');
+            })
+        })
+    })
+})
